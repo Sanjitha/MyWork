bd4ba476cef3e7f493868d2d1d4d706246e3a704
Sanjitha Udipi <sanjitha12345@gmail.com>
2016-02-28 20:26:55 -0500

    Javascript Closure:modification1

--- WebLanguages/Closures.js
+++
@@ -1,9 +0,0 @@
-var outerValue = "ninja";
-function outerFunction() {
-    var innerValue = "samurai";
-    function innerFunction()  {
-        assert(outerValue,"I can see ninja");
-        assert(innerValue,"I can see samurai");        
-    }
-    return innerFunction;
-}
