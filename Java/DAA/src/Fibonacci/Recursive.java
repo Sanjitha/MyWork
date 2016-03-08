8cc103ca018c6869d05b01c30a77ed518f4609d3
Sanjitha Udipi <sanjitha12345@gmail.com>
2016-03-08 15:03:17 -0500

    Hands on with D3

--- Java/DAA/src/Fibonacci/Recursive.java
+++ Java/DAA/src/Fibonacci/Recursive.java
@@ -1,17 +1,25 @@
 package Fibonacci;
 
+import java.util.Arrays;
+
 public class Recursive {
+	public static int[] fib;
 	public static void main(String args[]) {
 		int n = Main.getInputNumber();
-		
+		fib = new int[n];
+		Arrays.fill(fib, 0);
 		System.out.println("The fibonacci number of "+n+" is "+Fib(n));
 	}
 	public static int Fib(int n ) {
+		if(fib[n]!=0)
+			return fib[n];
 		if(n ==0) 
 				return 0;
 		else if(n ==1) 
 				return 1;
-		else 
-			return 	Fib(n-1)+Fib(n-2);
+		else {
+			fib[n]=	Fib(n-1)+Fib(n-2);
+			return fib[n];
+		}
 	}
 }
